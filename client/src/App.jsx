900ae38d2db0567728b0a05422d669d362ade25e
ayush08negi <negi08ayush@gmail.com>
2025-06-30 17:13:55 +0530

    home page completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import CarDetails from './pages/CarDetails';
 import Cars from './pages/Cars';
 import MyBooking from './pages/MyBooking';
+import Footer from './components/Footer';
 
 const  App = () => {
   const [showLogin , setShowLogin] = useState(false);
@@ -19,6 +20,11 @@ const  App = () => {
       <Route path = '/cars' element = {<Cars/>} />
       <Route path = '/my-bookings' element = {<MyBooking/>} />
      </Routes>
+
+    {
+      !isOwnerPath &&  <Footer/>
+    }
+    
 
     </>
   )
