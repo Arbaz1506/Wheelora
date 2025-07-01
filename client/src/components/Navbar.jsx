226075b487e19064f0cee23c5a3269ff44cff54a
ayush08negi <negi08ayush@gmail.com>
2025-07-01 11:50:55 +0530

    added myBooking page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = ({setShowLogin}) => {
         <div className={`max-sm:fixed max-sm:h-screen max-sm:w-full max-sm:top-16 max-sm:border-t border-borderColor right-0 flex flex-col sm:flex-row
         items-start sm:items-center gap-4 sm:gap-8 max-sm:p-4 transition-all duration-300 z-50 ${location.pathname === "/" ? "bg-light" : "bg-white"} ${open ? "max-sm:translate-x-0" : "max-sm:translate-x-full"}`}>
         {menuLinks.map((link,index)=>(
-            <Link key = {index} tp = {link.path}>
+            <Link key = {index} to = {link.path}>
                 {link.name}
             </Link>
         ))}
