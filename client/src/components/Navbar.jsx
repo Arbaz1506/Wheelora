18af65a5f2c0a12502db6e6ad0ecc75720c63fee
ayush08negi <negi08ayush@gmail.com>
2025-07-10 21:10:52 +0530

    animation added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import {assets,menuLinks} from '../assets/assets'
 import { useState } from 'react'
 import { useAppContext } from '../context/AppContext'
 import toast from 'react-hot-toast'
+import { motion } from 'motion/react'
 
 const Navbar = () => {
 
@@ -28,10 +29,14 @@ const Navbar = () => {
     }
 
   return (
-    <div className={`flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 text-gray-600 border-b border-borderColor relative transition-all 
+    < motion.div
+    initial={{y:-20, opacity:0}}
+    animate={{y:0, opacity:1}} 
+    transition={{duration:0.5}}
+    className={`flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 text-gray-600 border-b border-borderColor relative transition-all 
      ${location.pathname === "/" ? "bg-light" : "bg-white"}`}>
         <Link to='/'>
-        <img src={assets.logo} alt="logo" className='h-8' />
+        <motion.img  whileHover={{scale:1.05}} src={assets.logo} alt="logo" className='h-8' />
         </Link> 
 
         <div className={`max-sm:fixed max-sm:h-screen max-sm:w-full max-sm:top-16 max-sm:border-t border-borderColor right-0 flex flex-col sm:flex-row
@@ -59,7 +64,7 @@ const Navbar = () => {
         <img src={open ? assets.close_icon : assets.menu_icon } alt="menu" />
     </button>
 
-    </div>
+    </motion.div>
 
     
   )
